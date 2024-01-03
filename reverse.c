b94996adc4faaf19301baf0db8e845d22b389a1f
enejivk <enejivic@gmail.com>
2024-01-03 23:13:06 +0100

    print Reverse

--- reverse.c
+++ reverse.c
@@ -3,8 +3,10 @@
 int main(void)
 {
     char i;
-    for(i = 'z'; i >= 'a'; --i)
+    for(i = 'a'; i <= 'z'; i++)
     {
+        if(i == 'e' || i == 'q')
+        continue;
         putchar(i);
     }
     putchar('\n');
